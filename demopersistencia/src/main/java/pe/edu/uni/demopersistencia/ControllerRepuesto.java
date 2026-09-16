f85724bf1e9caed67b5c558839faa6e80cb627cc
Pedro David Huayanay Cardenas <pedro_davidhc@hotmail.com>
2026-09-16 03:03:35 +0000

    semana 6 implementacion de persistencia curso Desarrollo web Integrado

---
+++ demopersistencia/src/main/java/pe/edu/uni/demopersistencia/ControllerRepuesto.java
@@ -0,0 +1,26 @@
+package pe.edu.uni.demopersistencia;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class ControllerRepuesto {
+
+    @Autowired 
+    RepositoryRepuesto repoRepuesto;
+
+    @PostMapping("/repuesto/nuevo")
+    public Repuesto registrar(@RequestBody Repuesto nuevo) {
+        Repuesto r = repoRepuesto.save(nuevo);
+        return r;
+    }
+
+    @GetMapping("/repuesto/id/{id}")
+    public Repuesto consultarPorId(@PathVariable(name = "id") int id) {
+        return repoRepuesto.findById(id).get();
+    }
+}
---
+++ demopersistencia/src/main/java/pe/edu/uni/demopersistencia/RepositoryRepuesto.java
@@ -0,0 +1,8 @@
+package pe.edu.uni.demopersistencia;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryRepuesto extends JpaRepository<Repuesto, Integer> {
+
+}
+
